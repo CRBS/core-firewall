02c0e0ead61e1aba0d4ede0685a015f77888e951
Edmond Negado <enegado@rockstar.ucsd.edu>
2014-10-15 14:19:17 -0700

    added ssh

--- manifests/pre.pp
+++ manifests/pre.pp
@@ -14,9 +14,16 @@ class core_firewall::pre {
     iniface => 'lo',
     action  => 'accept',
   }->
-  firewall { '002 accept related established rules':
+  firewall { '002 accept port 22 tcp':
+    proto   => 'tcp',
+    port    => 22,
+    action  => 'accept',
+  }->
+  firewall { '003 accept related established rules':
     proto   => 'all',
     state   => ['RELATED', 'ESTABLISHED'],
     action  => 'accept',
   }
+
+
 }

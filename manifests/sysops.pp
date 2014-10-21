c3be6cfc43d373ce0570ab417d33ab1a702317d9
Edmond Negado <enegado@rockstar.ucsd.edu>
2014-10-21 09:47:29 -0700

    adding sysops.pp

---
+++ manifests/sysops.pp
@@ -0,0 +1,9 @@
+
+class core_firewall::sysops
+{
+  include core_firewall::sysops::spenticoff
+  include core_firewall::sysops::vrowley
+  include core_firewall::sysops::llui
+  include core_firewall::sysops::enegado
+}
+
